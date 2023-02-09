ffb5b0de848816fb7e21890a6440fa17dc3d0733
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-02-09 13:51:44 -0300

    Correcion nav contacto

--- js/nav_animation.js
+++ js/nav_animation.js
@@ -3,23 +3,35 @@ let texto_links = document.querySelectorAll("#texto_links");
 let logo_nav_pc = document.querySelector("#logo_nav_pc");
 let logo_nav_phone = document.querySelector("#logo_nav_phone");
 
-// Cambio de color de la barra de navegación al hacer scroll
+// Cambio la barra de navegación al hacer scroll
 
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 10) {
-    barra_nav.classList.add("bg-blanco");
+  if (this.innerWidth > 992) {
+    logo_nav_phone.className = "d-none";
+    logo_nav_pc.className = "";
+    if (window.pageYOffset > 10) {
+      barra_nav.classList.add("bg-blanco");
+      texto_links.forEach((elemento) => {
+        elemento.classList.add("texto-azul");
+      });
+      logo_nav_pc.src = "./img/logo.png";
+    } else {
+      barra_nav.classList.remove("bg-blanco");
+      texto_links.forEach((elemento) => {
+        elemento.classList.remove("texto-azul");
+      });
+      logo_nav_pc.src = "./img/logo.svg";
+    }
+  }else{
     texto_links.forEach((elemento) => {
-      elemento.classList.add("texto-azul");
+      elemento.classList.add("text-blanco");
     });
-    logo_nav_pc.src = "./img/logo.png";
-  } else {
-    barra_nav.classList.remove("bg-blanco");
-    texto_links.forEach((elemento) => {
-      elemento.classList.remove("texto-azul");
-    });
-    logo_nav_pc.src = "./img/logo.svg";
+    logo_nav_phone.className = "";
+    logo_nav_pc.className = "d-none";
   }
 });
+
+// Cambio la barra de navegación al cambiar el tamaño de la pantalla
 
 function ToggleLogoMediaQuery() {
   if (innerWidth < 992) {
